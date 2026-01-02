1ebc21114434ce103a913550e87e74ddb8cd56f4
Santiago Garcia Arango <san99tiago@gmail.com>
2026-01-02 15:45:04 -0500

    Add META properties for URL previews OG

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,30 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Open Graph Meta Tags */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://san99tiago.com/" />
+          <meta
+            property="og:title"
+            content="Santiago Garcia Arango - Solutions Architect AWS"
+          />
+          <meta
+            property="og:description"
+            content="Website for Santiago Garcia Arango (DevOps, AWS, GenAI) - Solutions Architect - Technology"
+          />
+          <meta
+            property="og:image"
+            content="https://san99tiago.com/images/SantiagoGarciaArango_Profile_MAIN.png"
+          />
+          <meta property="og:image:width" content="1920" />
+          <meta property="og:image:height" content="1080" />
+          <meta property="og:locale" content="es_ES" />
+          <meta
+            property="og:site_name"
+            content="Santiago Garcia Arango - Solutions Architect AWS"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
